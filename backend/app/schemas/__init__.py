08fdb7e44a286283fd763868decc346b26311792
Varetoo <alvaromi2005@gmail.com>
2025-11-13 18:17:20 +0100

    Add board schemas `imports` in schemas module

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .user import User
+from .user import UserCreate
+
+from .token import Token
+from .token import TokenData
+
+from .board import BoardCreate
+from .board import BoardUpdate
+from .board import Board
