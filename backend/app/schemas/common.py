f76a1c1bcc3d8c40b2ad61c16da4f7c3da3dc1fb
Varetoo <alvaromi2005@gmail.com>
2025-11-18 10:14:29 +0100

    Wrap all (user, board, list and card) subschemas in a dedicated module.

---
+++ backend/app/schemas/common.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+
+"""
+This file contains all the necessary subschemas to be maintained from a single location.
+These subschemas will be imported into the response models to be used inside of the main schemes.
+"""
+
+
+class UserSubschema(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+
+class BoardSubschema(BaseModel):
+    id: int
+    name: str
+    description: str | None
+
+    class Config:
+        from_attributes = True
+
+
+class ListSubschema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class CardSubschema(BaseModel):
+    id: int
+    name: str
+    text: str | None
+    is_done: bool
+
+    class Config:
+        from_attributes = True
