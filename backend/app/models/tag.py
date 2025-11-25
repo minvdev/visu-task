4615a6597879a811cdadc1396fa89b12184d74d6
Varetoo <alvaromi2005@gmail.com>
2025-11-25 11:01:27 +0100

    Define a new `Tag` model and a `card_tags` association table.

---
+++ backend/app/models/tag.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from ..db.database import Base
+
+card_tags = Table(
+    'card_tags',
+    Base.metadata,
+    Column('card_id', Integer, ForeignKey(
+        "cards.id", ondelete="CASCADE"), primary_key=True),
+    Column('tag_id', Integer, ForeignKey(
+        "tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=True)
+    color = Column(String(7), nullable=False)
+
+    board_id = Column(Integer, ForeignKey("boards.id"), nullable=False)
+    board = relationship("Board", back_populates="tags")
+
+    cards = relationship("Card", secondary=card_tags, back_populates="tags")
