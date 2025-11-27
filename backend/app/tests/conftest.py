9415ae24fea09f3dcf424b06da3ab6a2dc8b5837
Varetoo <alvaromi2005@gmail.com>
2025-11-27 12:05:11 +0100

    Define a new 'fill_data' fixture to get a ready to test database context.

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -6,6 +6,7 @@ from fastapi.testclient import TestClient
 
 from app.db.database import Base, get_db
 from app.main import app
+from app.models import Board, List, Card, Tag
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
@@ -70,3 +71,84 @@ def auth_headers(client):
     token = response.json()["access_token"]
 
     return {"Authorization": f"Bearer {token}"}
+
+
+@pytest.fixture(scope="function")
+def fill_data(client, auth_headers):
+    """
+    Create data for testing.
+    [Inbox]                         [First Board]                       [Second Board]
+
+    [Incoming]                      [List 1]    [List 2]                [List 3]
+
+    [Task 1] [Task 2] [Task 3]      [Task 4]    [Task 5] [Task 6]       [Task 7] [Task 8] [Task 9]
+    """
+    default_headers, client.headers = client.headers, auth_headers
+
+    def create_lists(board_id, list_names: list[str]) -> list[dict]:
+        """
+        Create `n` lists in the board where `n` is the number of names on the list.
+        Also checks if the list was created.
+        """
+        url = f"/boards/{board_id}/lists/"
+        created_lists = [client.post(
+            url, json={"name": name}).json() for name in list_names]
+
+        response = client.get(url)
+        assert [list_item["name"]
+                for list_item in response.json()] == list_names
+
+        return created_lists
+
+    def create_cards(board_id, list_id, card_names: list[str]):
+        """
+        Create `n` cards in the list where `n` is the number of names on the list.
+        Also checks if the card was created.
+        """
+        url = f"/boards/{board_id}/lists/{list_id}/cards"
+
+        for name in card_names:
+            client.post(url, json={"name": name})
+
+        response = client.get(url)
+        assert [card["name"] for card in response.json()] == card_names
+
+    cards_count = 1
+    lists_count = 1
+    # Inbox
+    inbox = client.get("/inbox").json()
+    inbox_id = inbox["id"]
+    incoming_id = inbox["lists"][0]["id"]
+
+    create_cards(inbox_id, incoming_id, [
+                 f"Task {i}" for i in range(cards_count, cards_count+3)])
+    cards_count += 3
+
+    # First Board
+    board = client.post("/boards", json={"name": "First Board"}).json()
+    board_id = board["id"]
+
+    lists = create_lists(
+        board_id, [f"List {i}" for i in range(lists_count, lists_count+2)])
+    lists_count += 2
+    list_ids = [l["id"] for l in lists]
+
+    create_cards(board_id, list_ids[0], [
+                 f"Task {i}" for i in range(cards_count, cards_count+1)])
+    cards_count += 1
+    create_cards(board_id, list_ids[1], [
+                 f"Task {i}" for i in range(cards_count, cards_count+2)])
+    cards_count += 2
+
+    # Second Board
+    board = client.post("/boards", json={"name": "Second Board"}).json()
+    board_id = board["id"]
+
+    lists = create_lists(
+        board_id, [f"List {i}" for i in range(lists_count, lists_count+1)])
+    list_ids = [l["id"] for l in lists]
+
+    create_cards(board_id, list_ids[0], [
+                 f"Task {i}" for i in range(cards_count, cards_count+3)])
+
+    client.headers = default_headers
