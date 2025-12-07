15f6d63805ff68cd530429224876558e10b0570d
Varetoo <alvaromi2005@gmail.com>
2025-12-07 13:21:28 +0100

    Fix AuthContext imports

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext/AuthProvider";
 import BaseView from "./pages/BaseView/BaseView";
 import {
 	BrowserRouter,
