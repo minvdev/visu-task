737bd70339e526e07297ceaf48128755373dd1a0
Varetoo <alvaromi2005@gmail.com>
2025-12-15 13:34:30 +0100

    Add MainLayout parent route to the App with protection against no-login requests.

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,6 +1,9 @@
 import { AuthProvider } from "./context/AuthContext/AuthProvider";
+import { MainLayout } from "./components/templates/MainLayout/MainLayout";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
+import { Navigate } from "react-router-dom";
+import { ProtectedRoute } from "./components/utility/ProtectedRoute";
 import {
 	BrowserRouter,
 	Routes,
@@ -18,6 +21,19 @@ function App() {
 						path="/register"
 						element={<RegisterPage />}
 					/>
+
+					<Route
+						element={
+							<ProtectedRoute>
+								<MainLayout />
+							</ProtectedRoute>
+						}
+					>
+						<Route
+							path="/dashboard"
+							element={<h1>Dashboard</h1>}
+						/>
+					</Route>
 				</Routes>
 			</AuthProvider>
 		</BrowserRouter>
