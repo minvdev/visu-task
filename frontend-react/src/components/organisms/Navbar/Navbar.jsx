d260383f8f875b826ce55b8f53e05fcfbdeaab2e
Varetoo <alvaromi2005@gmail.com>
2025-12-15 13:30:52 +0100

    Add `Navbar` component.

---
+++ frontend-react/src/components/organisms/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { Heading } from "../../atoms/Heading/Heading";
+import { IconWrapper } from "../../molecules/IconWrapper/IconWrapper";
+import { Input } from "../../atoms/Input/Input";
+import { Button } from "../../atoms/Button/Button";
+import { useAuth } from "../../../hooks/useAuth";
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+	const { logout, user } = useAuth();
+	const [searchQuery, setSearchQuery] = useState("");
+
+	const handleSearchChange = (event) => {
+		setSearchQuery(event.target.value);
+	};
+
+	return (
+		<nav className={styles.navbar}>
+			<div className={styles.logo}>
+				<Heading level={3}>VisuTask</Heading>
+			</div>
+
+			<div className={styles.searchBar}>
+				<IconWrapper src="src/assets/icons/search.svg">
+					<Input
+						placeholder="Buscar"
+						className="input-sm"
+						value={searchQuery}
+						onChange={handleSearchChange}
+					/>
+				</IconWrapper>
+			</div>
+
+			<div className={styles.actions}>
+				<IconWrapper
+					src="src/assets/icons/user.svg"
+					alt="User avatar"
+					className="no-border"
+				>
+					<p>{user.username || "Usuario"}</p>
+				</IconWrapper>
+
+				<Button
+					onClick={logout}
+					variant="danger"
+					className="button-sm"
+				>
+					Salir
+				</Button>
+			</div>
+		</nav>
+	);
+};
