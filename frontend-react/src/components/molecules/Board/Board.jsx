32d829f8a6c533a77022cff4b257949389c7b12a
Varetoo <alvaromi2005@gmail.com>
2025-12-18 21:15:17 +0100

    Add `Board`component

---
+++ frontend-react/src/components/molecules/Board/Board.jsx
@@ -0,0 +1,36 @@
+import styles from "./Board.module.css";
+import clsx from "clsx";
+import { Image } from "../../atoms/Image/Image";
+import { Heading } from "../../atoms/Heading/Heading";
+
+export const Board = ({
+	src,
+	name,
+	className,
+	...props
+}) => {
+	return (
+		<div
+			className={clsx(
+				styles.board,
+				src ? styles.complexBoard : styles.simpleBoard,
+				className
+			)}
+			{...props}
+		>
+			{src && (
+				<div className={styles.imageContainer}>
+					<Image
+						src={src}
+						variants={[!src && "defaultFilter"]}
+						className={clsx(styles.image)}
+					/>
+				</div>
+			)}
+
+			<Heading level={4} className={clsx(styles.title)}>
+				{name}
+			</Heading>
+		</div>
+	);
+};
