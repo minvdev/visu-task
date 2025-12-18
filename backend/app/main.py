282598247ba3d8d742b1d947e5e00191bbb8c59d
Varetoo <alvaromi2005@gmail.com>
2025-12-18 21:05:48 +0100

    Add CORS middleware for the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import auth, boards, inbox, cards, users
 
 
@@ -13,3 +14,15 @@ app.include_router(boards.router)
 app.include_router(inbox.router)
 app.include_router(cards.router)
 app.include_router(users.router)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
