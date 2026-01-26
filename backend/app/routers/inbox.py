ca0aeaaf2d427b4223d963c0556277ec6b374b3b
Varetoo <alvaromi2005@gmail.com>
2026-01-26 11:20:26 +0100

    Replace `Board` response schema in `get_inbox` route for `Inbox`

--- backend/app/routers/inbox.py
+++ backend/app/routers/inbox.py
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=schemas.Board)
+@router.get("/", response_model=schemas.Inbox)
 def get_inbox(
     db: Session = Depends(get_db),
     current_user: User = CurrentUserDep
