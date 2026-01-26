e4d76ae283e2a1d803cb759ff32d4f2f36edaece
Varetoo <alvaromi2005@gmail.com>
2026-01-26 16:16:15 +0100

    Cleanup `BoardPage` layout and styles for initial structure

--- frontend-react/src/pages/BoardPage/BoardPage.jsx
+++ frontend-react/src/pages/BoardPage/BoardPage.jsx
@@ -1,27 +1,14 @@
 import styles from "./BoardPage.module.css";
-import {
-	useParams,
-	useSearchParams,
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Heading } from "../../components/atoms/Heading/Heading";
-import { Link } from "react-router-dom";
 
 export const BoardPage = () => {
-	const { boardSlug } = useParams();
-	const [searchParams, _] = useSearchParams();
-	const id = searchParams.get("id");
+	const { boardId: boardIdParam } = useParams();
+	const boardId = parseInt(boardIdParam);
 
 	return (
-		<div className={styles.container}>
-			<Heading level={1}>
-				Board Page{" "}
-				<Link to="/dashboard">
-					<button type="button">Back to Dashboard</button>
-				</Link>
-			</Heading>
-
-			<Heading level={3}>BoardSlug: {boardSlug}</Heading>
-			<Heading level={3}>Id: {id}</Heading>
+		<div className={styles.mainContainer}>
+			
 		</div>
 	);
 };
