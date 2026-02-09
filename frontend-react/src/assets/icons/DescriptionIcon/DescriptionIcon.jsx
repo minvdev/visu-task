3bfe7360e871cf29ea4011002d30049f4cb84b13
Varetoo <alvaromi2005@gmail.com>
2026-02-09 19:36:19 +0100

    Add two new icons  `DescriptionIcon` and `DownIcon`

---
+++ frontend-react/src/assets/icons/DescriptionIcon/DescriptionIcon.jsx
@@ -0,0 +1,5 @@
+export const DescriptionIcon = ({ className }) => {
+	return (
+		<svg width="16" height="16" fill="currentColor" className={className} viewBox="0 0 24 24"><path fillRule="evenodd" d="M3 17h12a1 1 0 0 1 .117 1.993L15 19H3a1 1 0 0 1-.117-1.993zh12zm0-4h18a1 1 0 0 1 .117 1.993L21 15H3a1 1 0 0 1-.117-1.993zh18zm0-4h18a1 1 0 0 1 .117 1.993L21 11H3a1 1 0 0 1-.117-1.993zh18zm0-4h18a1 1 0 0 1 .117 1.993L21 7H3a1 1 0 0 1-.117-1.993zh18z" clipRule="evenodd"/></svg>
+	);
+};
---
+++ frontend-react/src/assets/icons/DownIcon/DownIcon.jsx
@@ -0,0 +1,5 @@
+export const DownIcon = ({ className }) => {
+	return (
+		<svg width="16" height="16" fill="currentColor" className={className} viewBox="0 0 24 24"><path fillRule="evenodd" d="M4.293 8.293a1 1 0 0 1 1.414 0L12 14.586l6.293-6.293a1 1 0 1 1 1.414 1.414l-7 7a1 1 0 0 1-1.414 0l-7-7a1 1 0 0 1 0-1.414" clipRule="evenodd"/></svg>
+	);
+};
