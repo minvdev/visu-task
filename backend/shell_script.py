e2b0f18c602d4c6765f34dd4d9d1e905838aaeaf
Varetoo <alvaromi2005@gmail.com>
2026-02-09 19:20:59 +0100

    Add Tag in shell scritp

--- backend/shell_script.py
+++ backend/shell_script.py
@@ -1,5 +1,5 @@
 from app.db.database import SessionLocal
-from app.models import User, Board, List, Card
+from app.models import User, Board, List, Card, Tag
 from app.security import get_password_hash, verify_password
 
 db = SessionLocal()
@@ -8,6 +8,6 @@ print("------------------------------------------------------------")
 print("Interactive Shell for VisuTask")
 print("------------------------------------------------------------")
 print(
-    f"Available items: db, {User.__name__}, {Board.__name__}, {List.__name__}, {Card.__name__}")
+    f"Available items: db, {User.__name__}, {Board.__name__}, {List.__name__}, {Card.__name__}, {Tag.__name__}")
 print(f"Utilities: {get_password_hash.__name__}, {verify_password.__name__}")
 print("------------------------------------------------------------")
