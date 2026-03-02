e06b5dc0638669ebf3ed538e2ff07f1b8b66ce47
Álvaro Martínez <minvdev@gmail.com>
2026-03-02 13:17:33 +0100

    Refactor `TaskList` to use the optional API from  the `EditableText`. Invert the functionality of the task edit button with the editable text.

--- frontend-react/src/components/molecules/TaskList/TaskList.jsx
+++ frontend-react/src/components/molecules/TaskList/TaskList.jsx
@@ -1,6 +1,8 @@
 import styles from "./TaskList.module.css";
 import clsx from "clsx";
 
+import { useState } from "react";
+
 import { Task } from "../../atoms/Task/Task";
 import { Checkbox } from "../../atoms/Checkbox/Checkbox";
 import { EditableText } from "../EditableText/EditableText";
@@ -15,12 +17,18 @@ export const TaskList = ({
 	onTaskDelete,
 	onTaskEdit,
 }) => {
+	const [editingId, setEditingId] = useState(null);
+
 	const handleToggleCheckbox = (id, is_done) => () => {
 		onTaskUpdate(id, { is_done });
 	};
 
 	const handleTaskTextEdit = (id) => (name) => {
 		onTaskUpdate(id, { name });
+	};
+
+	const handleDisplayClick = (id) => () => {
+		onTaskEdit(id);
 	};
 
 	if (!tasks) return;
@@ -52,12 +60,19 @@ export const TaskList = ({
 							onSave={handleTaskTextEdit(task.id)}
 							multiline={true}
 							maxInputLength={100}
+							isEditing={editingId === task.id}
+							onRequestEdit={handleDisplayClick(task.id)}
+							onEditingChange={(val) => {
+								if (!val) setEditingId(null);
+							}}
 						>
 							{task.name}
 						</EditableText>
 
 						<ButtonBase
-							onClick={() => onTaskEdit(task.id)}
+							onClick={() => {
+								setEditingId(task.id);
+							}}
 							className={styles.editBtn}
 						>
 							<EditIcon />
