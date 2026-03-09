f7923557b7bf216153c2a2bfd000b6ed6a2b5858
Álvaro Martínez <minvdev@gmail.com>
2026-03-09 10:40:52 +0100

    Add `tagColors` constant file with the colors for the tags.

---
+++ frontend-react/src/constants/tagColors.js
@@ -0,0 +1,157 @@
+export const tagColors = [
+	{
+		id: 1,
+		backgroundColor: "#baf3db",
+		color: "#164b35",
+	},
+	{
+		id: 2,
+		backgroundColor: "#f5e989",
+		color: "#533f04",
+	},
+	{
+		id: 3,
+		backgroundColor: "#fce4a6",
+		color: "#693200",
+	},
+	{
+		id: 4,
+		backgroundColor: "#ffd5d2",
+		color: "#5d1f1a",
+	},
+	{
+		id: 5,
+		backgroundColor: "#eed7fc",
+		color: "#48245d",
+	},
+
+	{
+		id: 6,
+		backgroundColor: "#4bce97",
+		color: "#164b35",
+	},
+	{
+		id: 7,
+		backgroundColor: "#ddb30e",
+		color: "#533f04",
+	},
+	{
+		id: 8,
+		backgroundColor: "#fca700",
+		color: "#693200",
+	},
+	{
+		id: 9,
+		backgroundColor: "#f87168",
+		color: "#5d1f1a",
+	},
+	{
+		id: 10,
+		backgroundColor: "#c97cf4",
+		color: "#48245d",
+	},
+
+	{
+		id: 11,
+		backgroundColor: "#1f845a",
+		color: "#fff",
+	},
+	{
+		id: 12,
+		backgroundColor: "#946f00",
+		color: "#fff",
+	},
+	{
+		id: 13,
+		backgroundColor: "#bd5b00",
+		color: "#fff",
+	},
+	{
+		id: 14,
+		backgroundColor: "#c9372c",
+		color: "#fff",
+	},
+	{
+		id: 15,
+		backgroundColor: "#964ac0",
+		color: "#fff",
+	},
+
+	{
+		id: 16,
+		backgroundColor: "#cfe1fd",
+		color: "#123363",
+	},
+	{
+		id: 17,
+		backgroundColor: "#c6edfb",
+		color: "#164555",
+	},
+	{
+		id: 18,
+		backgroundColor: "#d3f1a7",
+		color: "#37471f",
+	},
+	{
+		id: 19,
+		backgroundColor: "#fdd0ec",
+		color: "#50253f",
+	},
+	{
+		id: 20,
+		backgroundColor: "#dddee1",
+		color: "#1e1f21",
+	},
+
+	{
+		id: 21,
+		backgroundColor: "#669df1",
+		color: "#123363",
+	},
+	{
+		id: 22,
+		backgroundColor: "#6cc3e0",
+		color: "#164555",
+	},
+	{
+		id: 23,
+		backgroundColor: "#94c748",
+		color: "#37471f",
+	},
+	{
+		id: 24,
+		backgroundColor: "#e774bb",
+		color: "#50253f",
+	},
+	{
+		id: 25,
+		backgroundColor: "#8c8f97",
+		color: "#1e1f21",
+	},
+
+	{
+		id: 26,
+		backgroundColor: "#1868db",
+		color: "#fff",
+	},
+	{
+		id: 27,
+		backgroundColor: "#227d9b",
+		color: "#fff",
+	},
+	{
+		id: 28,
+		backgroundColor: "#5b7f24",
+		color: "#fff",
+	},
+	{
+		id: 29,
+		backgroundColor: "#ae4787",
+		color: "#fff",
+	},
+	{
+		id: 30,
+		backgroundColor: "#6b6e76",
+		color: "#fff",
+	},
+];
