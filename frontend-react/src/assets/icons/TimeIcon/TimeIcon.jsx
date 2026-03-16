d911842f0ebd5f016a5c81bd3c1b09f9056f5aaf
Álvaro Martínez <minvdev@gmail.com>
2026-03-16 19:53:29 +0100

    New icon: `TimeIcon`

---
+++ frontend-react/src/assets/icons/TimeIcon/TimeIcon.jsx
@@ -0,0 +1,5 @@
+export const TimeIcon = ({ className }) => {
+	return (
+		<svg width="16" height="16" fill="none" className={className} viewBox="0 0 24 24"><path fill="currentColor" d="M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11M3.007 12a8.993 8.993 0 1 0 17.986 0 8.993 8.993 0 0 0-17.986 0"/><path fill="currentColor" d="M12 5a1 1 0 0 0-1 1v6.467s0 .26.127.457a1 1 0 0 0 .39.41l4.62 2.668a1 1 0 0 0 1-1.732L13 11.88V6a1 1 0 0 0-1-1"/></svg>
+	);
+};
