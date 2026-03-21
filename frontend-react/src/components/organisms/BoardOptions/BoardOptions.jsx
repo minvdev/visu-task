fbaac5a42a3e58f99a65d091807f43d4ec7175c5
Álvaro Martínez <minvdev@gmail.com>
2026-03-21 11:55:46 +0100

    `BoardOptions`: Expand menu to interact with tags. The board menu now have the capacibility to: - Show tags - Edit tags - Create new tags

--- frontend-react/src/components/organisms/BoardOptions/BoardOptions.jsx
+++ frontend-react/src/components/organisms/BoardOptions/BoardOptions.jsx
@@ -1,25 +1,87 @@
 import styles from "./BoardOptions.module.css";
 import clsx from "clsx";
 
-import { useEffect, useRef, useState } from "react";
+import {
+	useEffect,
+	useRef,
+	useState,
+	useLayoutEffect,
+} from "react";
 
 import { Popover } from "../../atoms/Popover/Popover";
 import { OptionMenu } from "../OptionMenu/OptionMenu";
 import { Heading } from "../../atoms/Heading/Heading";
+import { CreateTagForm } from "../CreateTagForm/CreateTagForm";
+import { EditTagForm } from "../EditTagForm/EditTagForm";
+import { TagList } from "../TagList/TagList";
 
 export const BoardOptions = ({
 	isOpen,
 	onClose,
 	ignoreElements,
 	onDeleteBoard,
+	tags,
+	onTagCreate,
+	onTagEdit,
+	onTagDelete,
 	className,
 }) => {
 	const [activePopovers, setActivePopovers] =
 		useState(null);
+	const [activeTag, setActiveTag] = useState(null);
+	const [formStyle, setFormStyle] = useState(null);
 
 	const containerRef = useRef(null);
 
+	const getHeightToEnd = (ref) => {
+		const windowHeight = window.innerHeight;
+		const refBottom =
+			ref.current.getBoundingClientRect().bottom;
+
+		return windowHeight - refBottom;
+	};
+
+	const setExclusiveTrue = (stateObject, activeKey) => {
+		return Object.keys(stateObject).reduce((acc, key) => {
+			acc[key] = key === activeKey;
+			return acc;
+		}, {});
+	};
+
+	const displayTags = () => {
+		setActivePopovers(
+			setExclusiveTrue(activePopovers, "isShowingTags"),
+		);
+	};
+
+	const displayMenu = () => {
+		setActivePopovers(
+			setExclusiveTrue(activePopovers, "isShowingMenu"),
+		);
+	};
+
+	const displayCreateTag = () => {
+		setActivePopovers(
+			setExclusiveTrue(activePopovers, "isCreatingTag"),
+		);
+	};
+
+	const displayEditTag = (tag) => {
+		setActiveTag(tag);
+		setActivePopovers(
+			setExclusiveTrue(activePopovers, "isEditingTag"),
+		);
+	};
+
 	const options = [
+		{
+			options: [
+				{
+					label: "Etiquetas",
+					onClick: displayTags,
+				},
+			],
+		},
 		{
 			options: [
 				{
@@ -34,13 +96,33 @@ export const BoardOptions = ({
 		const setupPop = () => {
 			setActivePopovers({
 				isShowingMenu: true,
+				isShowingTags: false,
+				isCreatingTag: false,
+				isEditingTag: false,
 			});
 		};
 		setupPop();
 	}, [isOpen]);
 
+	useLayoutEffect(() => {
+		if (!containerRef.current) return;
+		const calcStyles = {
+			"--maxFormHeight": `calc(${getHeightToEnd(containerRef)}px - var(--space-11))`,
+		};
+
+		const setStyles = () => {
+			setFormStyle(calcStyles);
+		};
+
+		setStyles();
+	}, [tags, containerRef]);
+
 	return (
-		<div className={className} ref={containerRef}>
+		<div
+			className={className}
+			ref={containerRef}
+			style={formStyle}
+		>
 			{isOpen && (
 				<Popover
 					onClose={onClose}
@@ -54,6 +136,34 @@ export const BoardOptions = ({
 								<Heading level={4}>Acciones</Heading>
 							}
 							options={options}
+						/>
+					)}
+
+					{activePopovers.isShowingTags && (
+						<TagList
+							tags={tags}
+							onClose={displayMenu}
+							onOpenCreateTag={displayCreateTag}
+							onOpenEditTag={displayEditTag}
+							className={styles.form}
+						/>
+					)}
+
+					{activePopovers.isCreatingTag && (
+						<CreateTagForm
+							onSubmit={onTagCreate}
+							onClose={displayTags}
+							className={styles.form}
+						/>
+					)}
+
+					{activePopovers.isEditingTag && (
+						<EditTagForm
+							onSubmit={onTagEdit}
+							onDelete={onTagDelete}
+							tag={activeTag}
+							onClose={displayTags}
+							className={styles.form}
 						/>
 					)}
 				</Popover>
