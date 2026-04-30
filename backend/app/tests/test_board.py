4ad71cbcd534c704a5351ab5b40a1f68769cf50e
Álvaro Martínez <minvdev@gmail.com>
2026-04-30 14:06:48 +0200

    Fix wrong comments section in test_board

--- backend/app/tests/test_board.py
+++ backend/app/tests/test_board.py
@@ -466,7 +466,7 @@ def test_card_cascade(client, auth_headers, db_session, fill_data):
     check_models_count(db, expected_models_count)
 
 
-# --- CARDS TESTS ---
+# --- TAGS TESTS ---
 
 def test_create_and_get_tags(client, auth_headers):
     # 1. Setup: Create board
@@ -527,7 +527,7 @@ def test_update_and_delete_tag(client, auth_headers):
         headers=auth_headers
     ).json()["id"]
 
-    # 2. Update List
+    # 2. Update Tag
     all_tags = client.patch(
         f"/boards/{board_id}/tags/{tag_id}",
         json={"name": "Super Important"},
@@ -536,7 +536,7 @@ def test_update_and_delete_tag(client, auth_headers):
     assert all_tags.status_code == 200
     assert all_tags.json()["name"] == "Super Important"
 
-    # 3. Delete List
+    # 3. Delete Tag
     all_tags = client.delete(
         f"/boards/{board_id}/tags/{tag_id}", headers=auth_headers)
     assert all_tags.status_code == 204
