b98a539a2dba520ba99ab83c7543054df52b98ba
Álvaro Martínez <minvdev@gmail.com>
2026-08-07 12:05:20 +0200

    Refactor (LoginPage): Simplify component moving submit logic to LoginForm component

--- frontend-react/src/pages/LoginPage/LoginPage.tsx
+++ frontend-react/src/pages/LoginPage/LoginPage.tsx
@@ -1,43 +1,12 @@
-import { useState } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext/AuthProvider";
 
 import { LoginForm } from "./components/LoginForm/LoginForm";
 
 export const LoginPage = () => {
-	const navigate = useNavigate();
-	const { login, user } = useAuth();
-
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-
-	const handleSubmit = async (credentials: {
-		username: string;
-		password: string;
-	}) => {
-		setIsLoading(true);
-		setError(null);
-
-		try {
-			await login(
-				credentials.username,
-				credentials.password,
-			);
-			navigate("/dashboard");
-		} catch (error) {
-			setError("Usuario o contraseña incorrectos");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+	const { user } = useAuth();
 
 	if (user) return <Navigate to="/" />;
 
-	return (
-		<LoginForm
-			onSubmit={handleSubmit}
-			isLoading={isLoading}
-			error={error}
-		/>
-	);
+	return <LoginForm />;
 };
