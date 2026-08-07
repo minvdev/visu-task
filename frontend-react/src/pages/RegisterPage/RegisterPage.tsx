d55b6d6d3ed9f384a90237126d3fccff2c16298c
Álvaro Martínez <minvdev@gmail.com>
2026-08-07 12:58:43 +0200

    Refactor (LoginPage): Simplify component, the submit logic now belongs to the LoginForm component

--- frontend-react/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend-react/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,47 +1,12 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { authService } from "@services/auth";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext/AuthProvider";
 
 import { RegisterForm } from "./components/RegisterForm/RegisterForm";
 
 export const RegisterPage = () => {
-	const navigate = useNavigate();
+	const { user } = useAuth();
 
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+	if (user) return <Navigate to="/" />;
 
-	const handleSubmit = async (credentials: {
-		username: string;
-		email: string;
-		password: string;
-	}) => {
-		setIsLoading(true);
-		setError(null);
-		const { username, email, password } = credentials;
-
-		try {
-			await authService.register({
-				username,
-				email,
-				password,
-			});
-			navigate("/login");
-		} catch (error) {
-			console.log(error);
-			setError(
-				error.message ||
-					"Error al conectar con el servidor",
-			);
-		} finally {
-			setIsLoading(false);
-		}
-	};
-
-	return (
-		<RegisterForm
-			onSubmit={handleSubmit}
-			isLoading={isLoading}
-			error={error}
-		/>
-	);
+	return <RegisterForm />;
 };
