6524d224c8a4b247c4eb968c580b0059c4b526e2
Álvaro Martínez <minvdev@gmail.com>
2026-08-12 12:34:32 +0200

    Refactor(FormField): Add errorKey prop to Error component for better ux

--- frontend-react/src/components/molecules/FormField/FormField.tsx
+++ frontend-react/src/components/molecules/FormField/FormField.tsx
@@ -3,18 +3,23 @@ import styles from "./FormField.module.css";
 import { Label } from "@atoms/Label/Label";
 import { Error } from "@components/atoms/Error/Error";
 
-import { type ComponentPropsWithRef } from "react";
+import {
+	type ComponentPropsWithRef,
+	type Key,
+} from "react";
 
 export interface FormFieldProps extends ComponentPropsWithRef<"div"> {
 	label: string;
 	htmlFor?: string | undefined;
 	error?: string | undefined;
+	errorKey?: Key | null | undefined;
 }
 
 export const FormField = ({
 	label,
 	htmlFor,
 	error,
+	errorKey,
 	children,
 	className,
 	...props
@@ -29,6 +34,7 @@ export const FormField = ({
 				<Error
 					message={error}
 					className={styles["error"]}
+					key={errorKey}
 				/>
 			)}
 		</div>
